afaeafad68091b275a77a419db83c4c98d4339dd
ruandao <ljy080829@gmail.com>
2018-12-28 17:32:50 +0800

    自动编译脚本

--- git-push-build-auto.sh
+++ git-push-build-auto.sh
@@ -11,7 +11,25 @@ if [ $? -ne 0 ];then
     exit 2
 fi
 
-buildV=$(date "+%Y-%m-%d_%H_%M_%S")
+
+# now 变量从上层传进来，如果未设置，说明不是开发版本，不是释放版本
+now=$1
+if [ -z $now ];then
+    echo "develop version"
+    now=`date +"dev_%y-%m-%d_%H_%M_%S"`
+
+else
+    echo "release version"
+fi
+
+./xxxx $now
+# 如果 $? 不是 0 说明 xxx 脚本调用失败
+if [ $? -ne 0 ];then
+    echo "调用脚本 xxx 失败"
+    exit 1
+fi
+
+buildV=$now
 tagName="build_$buildV"
 git tag $tagName
 git push
