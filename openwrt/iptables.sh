6ad917db8eec86f5c4cd170bf35fb026994de1c5
软刀 <ljy080829@gmail.com>
2019-04-13 18:49:46 +0800

    openwrt

--- openwrt/iptables.sh
+++ openwrt/iptables.sh
@@ -9,25 +9,25 @@ start() {
     iptables -t nat -N chinadnsproxy
 
     # 远程 vps 流量直接放行
-    iptables -t nat -A SHADOWSOCKS -d 3.112.68.240 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 47.91.210.143 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 3.112.68.240 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 47.91.210.143 -j RETURN
 
     # 内网流量直接放行
-    iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
-    iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 0.0.0.0/8 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 10.0.0.0/8 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 127.0.0.0/8 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 169.254.0.0/16 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 172.16.0.0/12 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 192.168.0.0/16 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 224.0.0.0/4 -j RETURN
+    iptables -t nat -A chinadnsproxy -d 240.0.0.0/4 -j RETURN
 
     # 国内流量直接放行
     for ip in `cat /etc/chinadns_chnroute.txt`;do
-        iptables -t nat -A SHADOWSOCKS -d $ip -j RETURN;
+        iptables -t nat -A chinadnsproxy -d $ip -j RETURN;
     done
     # 剩下的转发到代理端口
-    iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 1235;
+    iptables -t nat -A chinadnsproxy -p tcp -j REDIRECT --to-ports 1235;
 }
 
 stop() {
