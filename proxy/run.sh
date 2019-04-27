3f5885604c4f39d800c38a560a4af778c4ce2c42
yan <yan@localhost.localdomain>
2019-04-27 15:02:34 -0400

    add git proxy in proxy/run.sh

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+git config --global http.proxy 'socks://127.0.0.1:2234'
+git config --global https.proxy 'socks://127.0.0.1:2234'
 docker swarm init
 docker stack deploy -c docker-compose.yml www
