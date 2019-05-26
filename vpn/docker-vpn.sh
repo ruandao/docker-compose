bd2c3d3a1eed26192d72b70fdcc21249cd2f3f44
ruandao <ljy080829@gmail.com>
2019-05-26 16:43:47 +0800

    m

---
+++ vpn/docker-vpn.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo modprobe af_key
+
+docker run \
+    --name ipsec-vpn-server \
+    --restart=always \
+    -p 500:500/udp \
+    -p 4500:4500/udp \
+    -v "$(pwd)/vpn.env:/opt/src/vpn.env:ro" \
+    -v /lib/modules:/lib/modules:ro \
+    -d --privileged \
+    hwdsl2/ipsec-vpn-server
