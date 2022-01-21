366f57cf0cc00b3cf49c100550c74a89a8791871
软刀 <ljy080829@gmail.com>
2022-01-21 16:01:44 +0800

    docker 自动化运行例子

---
+++ docker/自动化脚本范例/nodejs/runDocker.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "文件被修改，请先清空变化";
+  exit 1;
+else
+  echo "no changes";
+fi
+
+d=`git show -s --format=%cd --date=format:'%Y-%m-%d_%H_%M_%S'`
+hash=`git rev-parse --short HEAD`
+imgName=qjz:${d}_${hash}
+echo "容器名为: $imgName"
+
+# 生成镜像
+docker build -t $imgName .
+
+# 清理旧的容器服务
+docker rm -f qjz
+# 运行容器
+docker run -d --name qjz --env-file ../env/qjz -p 7002:7002 -p 7022:7022  -v `pwd`/logs:/root/logs $imgName
+
