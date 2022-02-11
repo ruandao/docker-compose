62e3273ef59afc8e56e53e9156436380d08f48db
软刀 <ljy080829@gmail.com>
2022-02-11 16:21:39 +0800

    sonarqube

--- sonarqube/runInMac.sh
+++ sonarqube/runInMac.sh
@@ -8,5 +8,5 @@ mkdir -p /Users/cpu/dev/docker/sonarqube/postgresql/db
 mkdir -p /Users/cpu/dev/docker/sonarqube/postgresql/data
 # mkdir -p 
 
-docker stack deploy -c docker-compose-mac.yml codeReview
+docker stack deploy --platform=linux/amd64 -c docker-compose-mac.yml codeReview
 # docker stack rm codeReview
